99ba5d536ebf8a19aedcf7a3adbe67d03a20c2ee
Baptiste Cluzel <baptiste.clu@gmail.com>
2024-09-05 10:02:38 +0200

    Update config

--- keyboards/sofle_choc/keymaps/vial/config.h
+++ keyboards/sofle_choc/keymaps/vial/config.h
@@ -16,14 +16,6 @@
 #pragma once
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-// Enabling this option changes the startup behavior to listen for an
-// active USB communication to delegate which part is master and which
-// is slave. With this option enabled and theres’s USB communication,
-// then that half assumes it is the master, otherwise it assumes it
-// is the slave.
-//
-// I've found this helps with some ProMicros where the slave does not boot
-#define SPLIT_USB_DETECT
 
 #define SPLIT_TRANSPORT_MIRROR             // If LED_MATRIX_KEYPRESSES or LED_MATRIX_KEYRELEASES is enabled, you also will want to enable SPLIT_TRANSPORT_MIRROR
 #define SPLIT_WPM_ENABLE
